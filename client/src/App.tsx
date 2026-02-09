79820d54b6c718d374870d18ea60a84a266706ac
HSG116 <your-email@example.com>
2026-02-09 04:27:36 +0200

    Deploy to main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -116,7 +116,7 @@ function App() {
       <AuthProvider>
         <CartProvider>
           <TooltipProvider>
-            <WouterRouter>
+            <WouterRouter base="/almarah">
               <Toaster />
               <NotificationManager />
               <Router />
